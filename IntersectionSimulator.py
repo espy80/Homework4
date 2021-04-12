e1e349676f34bb00d28787d57a537f9ee8517da8
Derek Babb <dvbabb@unomaha.edu>
2021-04-12 10:31:36 -0500

    Updated with roundabout

--- IntersectionSimulator.py
+++ IntersectionSimulator.py
@@ -10,19 +10,23 @@ hourlyRates = [0.0081, 0.0052, 0.0047, 0.0057, 0.0099, 0.0230, 0.0489, 0.0679, 0
 # From the Omaha Traffic Count PDF
 # 67th & Pacific 31,938 8,752 13,514 18,594 23,016 09/17
 # 50th & Underwood Ave 12,816 4,856 4,432 7,270 9,074
-north = 8752//2
-south = 13514//2
-east = 18594//2
-west = 23016//2
+eadt = 12816
+# Numbers for NSEW are every car to pass through
+# A car traveling North to South would get counted in each value.
+
+north = 4856
+south = 4432
+east = 7270
+west = 9074
 
 # Intersection Configuration
-EW_Lanes = 4
+EW_Lanes = 2
 NS_Lanes = 2
 
 EW_Green = 60
 NS_Green = 45
 green_direction = "EW"
-# Control Device can be a "Light" or "Sign"
+# Control Device can be a "Light", "Sign", or "Roundabout"
 # Stop signs cannot be used for intersections with more than 2 lanes
 controlDevice = "Light"
 departRate = 2 #how quickly can they go once it is their turn in seconds.
@@ -45,6 +49,7 @@ southCars = []
 carCount = 0
 departCount = 0
 signOrder = []
+roundBlocked = {"N":[],"S":[], "E":[], "W":[]}
 
 # Simulation for the traffic light. For simplicity, yellow is ignored.
 def traffic_light(env):
@@ -70,27 +75,23 @@ def arrive(env, dir):
         carCount += 1
         hour = int(env.now // SECONDS_PER_HOUR)
         logInfo = (carCount, env.now, dir)
-        #print(len(signOrder))
         if len(signOrder) < 4:
-            #print("added", dir)
             try:
                 signOrder.index(dir)
             except Exception as e:
                 signOrder.append(dir)
         if dir == "N":
             northCars.append(logInfo)
-            arrivalRate = int(SECONDS_PER_HOUR / (north * hourlyRates[hour])  + .5)
+            arrivalRate = int(SECONDS_PER_HOUR / (north/2 * hourlyRates[hour])  + .5)
         if dir == "W":
             westCars.append(logInfo)
-            arrivalRate = int(SECONDS_PER_HOUR / (west * hourlyRates[hour])  + .5)
+            arrivalRate = int(SECONDS_PER_HOUR / (west/2 * hourlyRates[hour])  + .5)
         if dir == "S":
             southCars.append(logInfo)
-            arrivalRate = int(SECONDS_PER_HOUR / (south * hourlyRates[hour]) + .5)
+            arrivalRate = int(SECONDS_PER_HOUR / (south/2 * hourlyRates[hour]) + .5)
         if dir == "E":
             eastCars.append(logInfo)
-            arrivalRate = int(SECONDS_PER_HOUR / (east * hourlyRates[hour]) + .5)
-
-
+            arrivalRate = int(SECONDS_PER_HOUR / (east/2 * hourlyRates[hour]) + .5)
 
         # determine delay time until the next car by the current hour
         yield env.timeout(arrivalRate)
@@ -100,7 +101,6 @@ def departSign(env):
     Function that determines who is going to depart the stop sign.
     """
     while True:
-        #print(signOrder)
         if len(signOrder) == 0:
             pass
         else:
@@ -128,6 +128,60 @@ def departSign(env):
                     signOrder.append("E")
 
         yield env.timeout(departRate)
+
+
+def departRoundabout(env, dir):
+    """
+    Function that determines who is going to depart the roundabout.
+    """
+    while True:
+        #check to see if you can go.
+        #as a car enters, it will close off the lane to it's right for a (duration)
+        while len(roundBlocked[dir]) > 0:
+            #Roundabout is full, wait one time unit and check again.
+            yield env.timeout(1)
+
+
+        if dir == "N":
+            if len(northCars) > 0:
+                car = northCars.pop(0)
+                eventLog.append((car[0], car[1], env.now, car[2]))
+                roundBlocked["W"].append(car[0]) #add self to blocked list.
+                yield env.timeout(departRate)
+                roundBlocked["W"].remove(car[0]) #remove self from blocked list.
+            else:
+                yield env.timeout(1)
+
+        if dir == "W":
+            if len(westCars) > 0:
+                car = westCars.pop(0)
+                eventLog.append((car[0], car[1], env.now, car[2]))
+                roundBlocked["S"].append(car[0]) #add self to blocked list.
+                yield env.timeout(departRate)
+                roundBlocked["S"].remove(car[0]) #remove self from blocked list.
+            else:
+                yield env.timeout(1)
+
+        if dir == "S":
+            if len(southCars) > 0:
+                car = southCars.pop(0)
+                eventLog.append((car[0], car[1], env.now, car[2]))
+                roundBlocked["E"].append(car[0]) #add self to blocked list.
+                yield env.timeout(departRate)
+                roundBlocked["E"].remove(car[0]) #remove self from blocked list.
+            else:
+                yield env.timeout(1)
+
+        if dir == "E":
+            if len(eastCars) > 0:
+                car = eastCars.pop(0)
+                eventLog.append((car[0], car[1], env.now, car[2]))
+                roundBlocked["N"].append(car[0]) #add self to blocked list.
+                yield env.timeout(departRate)
+                roundBlocked["N"].remove(car[0]) #remove self from blocked list.
+            else:
+                yield env.timeout(1)
+
 
 
 def departLight(env):
@@ -161,16 +215,23 @@ def departLight(env):
 
 
 def main():
+
+    control = controlDevice.lower()
     env = simpy.Environment()
     env.process(arrive(env, "N"))
     env.process(arrive(env, "S"))
     env.process(arrive(env, "E"))
     env.process(arrive(env, "W"))
-    if controlDevice == "Light":
+    if control == "light":
         env.process(traffic_light(env))
         env.process(departLight(env))
-    else:
+    elif control == "sign":
         env.process(departSign(env))
+    elif control == "roundabout":
+        env.process(departRoundabout(env, "N"))
+        env.process(departRoundabout(env, "S"))
+        env.process(departRoundabout(env, "E"))
+        env.process(departRoundabout(env, "W"))
 
     env.run(until=SECONDS_PER_DAY)
 
